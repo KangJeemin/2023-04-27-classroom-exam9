505dc9d42ce91681e86dbcc5bcc2a96d013355bc
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-04-27 23:24:04 +0900

    일단 어제 썼던 예제 가져오자

--- 강지민.js
+++ 강지민.js
@@ -1,1 +1,38 @@
 import inquirer from "inquirer";
+
+//? 짝수 엘레베이터는 1층도 가능함
+//? 홀수 엘레베이터는 10층도 가능함
+//? 짝수 홀수 일레베이터 둘다 지하일층 -1까지 가능하게 설계
+
+
+
+function oddElevator(floorNumber) {
+
+}
+
+function evenElevator(floorNumber){
+
+}
+
+inquirer
+    .prompt(exampleOne([
+        {
+            type:'input',
+            name:'name',
+            message:"당신의 이름은 무엇입니까?"
+        },
+        {
+            type:'list',
+            name:'like food',
+            message: '당신이 좋아하는 음식은 무엇입니까?',
+            choices:['바나나우유','딸기우유','초코우유','그냥우유'],
+        },
+        {
+            type:'confirm',
+            name:'confirm',
+            message:'확실합니까?'
+        },
+    ]))
+    .then((answers)=>{
+        console.log('Answers:',answers);
+    });
